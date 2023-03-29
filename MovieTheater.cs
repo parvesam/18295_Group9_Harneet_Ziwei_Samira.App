81baf85cfa2cfb5462482195688bd736a7e3d2b8
parvesam <parvesam@sheridancollege.ca>
2023-03-29 12:40:26 -0400

    i put the no.of seats for each movie

--- MovieTheater.cs
+++ MovieTheater.cs
@@ -15,16 +15,16 @@ namespace _18295_Group9_Harneet_Ziwei_Samira
         {
             Movies = new List<Movie>()
         {
-            new Movie("Avengers: Endgame", "Action", "New York", 100, new DateTime(2023, 3, 23, 12, 0, 0)),
-            new Movie("The Lion King", "Animation", "Los Angeles", 80, new DateTime(2023, 3, 23, 14, 0, 0)),
-            new Movie("Joker", "Drama", "Chicago", 50, new DateTime(2023, 3, 23, 16, 0, 0)),
-            new Movie("Star Wars: The Rise of Skywalker", "Sci-Fi", "Houston", 70, new DateTime(2023, 3, 23, 18, 0, 0)),
-            new Movie("Spider-Man: No Way Home", "Action", "New York", 60, new DateTime(2023, 3, 23, 20, 0, 0)),
-            new Movie("Black Panther", "Action", "Chicago", 90, new DateTime(2023, 3, 24, 12, 0, 0)),
-            new Movie("Frozen 2", "Animation", "Los Angeles", 80, new DateTime(2023, 3, 24, 14, 0, 0)),
-            new Movie("Interstellar", "Sci-Fi", "Houston", 50, new DateTime(2023, 3, 24, 16, 0, 0)),
-            new Movie("The Godfather", "Drama", "New York", 70, new DateTime(2023, 3, 24, 18, 0, 0)),
-            new Movie("Titanic", "Romance", "Chicago", 60, new DateTime(2023, 3, 24, 20, 0, 0))
+            new Movie("Avengers: Endgame", "Action", "New York", 100, new DateTime(2023, 3, 23, 12, 0, 0), 73),
+            new Movie("The Lion King", "Animation", "Los Angeles", 80, new DateTime(2023, 3, 23, 14, 0, 0), 42),
+            new Movie("Joker", "Drama", "Chicago", 50, new DateTime(2023, 3, 23, 16, 0, 0), 88),
+            new Movie("Star Wars: The Rise of Skywalker", "Sci-Fi", "Houston", 70, new DateTime(2023, 3, 23, 18, 0, 0), 102),
+            new Movie("Spider-Man: No Way Home", "Action", "New York", 60, new DateTime(2023, 3, 23, 20, 0, 0), 67),
+            new Movie("Black Panther", "Action", "Chicago", 90, new DateTime(2023, 3, 24, 12, 0, 0), 81),
+            new Movie("Frozen 2", "Animation", "Los Angeles", 80, new DateTime(2023, 3, 24, 14, 0, 0), 95),
+            new Movie("Interstellar", "Sci-Fi", "Houston", 50, new DateTime(2023, 3, 24, 16, 0, 0), 88),
+            new Movie("The Godfather", "Drama", "New York", 70, new DateTime(2023, 3, 24, 18, 0, 0), 53),
+            new Movie("Titanic", "Romance", "Chicago", 60, new DateTime(2023, 3, 24, 20, 0, 0), 62)
         };
         }
     }
