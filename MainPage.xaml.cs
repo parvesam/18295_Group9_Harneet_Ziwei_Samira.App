12b2909aeaf707dab6d9f2ffb0dad4e826bc480f
parvesam <parvesam@sheridancollege.ca>
2023-04-04 09:32:17 -0400

    fixed errors

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,20 +12,9 @@ public partial class MainPage : ContentPage
 
     }
 
-
-    void SearchEntry_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        MoviesListView.ItemsSource = MoviesRepository.SearchByName(SearchEntry.Text);
-    }
-    void MoviesListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-    {
-        var selectedMovie = MoviesListView.SelectedItem as Movie;
-<<<<<<< HEAD
-        Navigation.PushAsync(new MoviePage(selectedMovie));
-=======
-        Navigation.PushAsync(new BookTicket(selectedMovie));
->>>>>>> 4fd0432d9a74c8a45953cb819b8e74c189f1521d
-    }
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
 
     
 }
