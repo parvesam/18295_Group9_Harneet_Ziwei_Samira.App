fc73b3b41eea1fe9536fa65e9dd1e9cb79275007
Frank <zhaoziwei0@gmail.com>
2023-04-16 21:18:25 -0400

    updates done by...

--- Pages/SeatReservation.xaml.cs
+++ Pages/SeatReservation.xaml.cs
@@ -1,15 +1,19 @@
 using _18295_Group9_Harneet_Ziwei_Samira.Models;
 using System;
+using Microsoft.Maui.Graphics;
 
 namespace _18295_Group9_Harneet_Ziwei_Samira.Pages;
 
 public partial class SeatReservation : ContentPage
 {
-   
-	public SeatReservation(Hall hall)
+    private Hall _hall;
+    private bool[] _selectedSeats;
+
+    public SeatReservation(Hall hall)
 	{
         InitializeComponent();
-		
+        _hall = hall;
+        _selectedSeats = new bool[_hall.NumberOfSeats];
         Hall.PopulateMap(hall.NumberOfSeats);
         
 
@@ -18,11 +22,31 @@ public partial class SeatReservation : ContentPage
 
     private void ImageButton_Clicked(object sender, SelectedItemChangedEventArgs e)
     {
+        ImageButton button = sender as ImageButton;
+        int row = Grid.GetRow(button);
+        int column = Grid.GetColumn(button);
+        int seatIndex = row * 4 + column;
+
+        if (_selectedSeats[seatIndex])
+        {
+            button.BackgroundColor = Colors.Blue;
+            _selectedSeats[seatIndex] = false;
+        }
+        else
+        {
+            button.BackgroundColor = Colors.Red;
+            _selectedSeats[seatIndex] = true;
+        }
 
     }
 
     private void ImageButton_Clicked(object sender, EventArgs e)
     {
 
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+       // Navigation.PushAsync();
     }
 }
