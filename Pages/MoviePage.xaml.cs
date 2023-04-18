44b43b350e57621c9e39fda7a467faf6770070ec
parvesam <parvesam@sheridancollege.ca>
2023-04-18 12:35:13 -0400

    made changes to the movie page xaml and xaml.cs

--- Pages/MoviePage.xaml.cs
+++ Pages/MoviePage.xaml.cs
@@ -1,16 +1,30 @@
-//using _18295_Group9_Harneet_Ziwei_Samira.Models;
-//namespace _18295_Group9_Harneet_Ziwei_Samira;
+using _18295_Group9_Harneet_Ziwei_Samira.Models;
+using _18295_Group9_Harneet_Ziwei_Samira.Pages;
+namespace _18295_Group9_Harneet_Ziwei_Samira;
+public partial class MoviePage : ContentPage
+{
+    private Movie _movies;
+    public MoviePage(Movie movie)
+    {
+        InitializeComponent();
+        _movies = movie;
 
-//public partial class MoviePage : ContentPage
-//{
-//    private Movie _movie;
-//    public MoviePage(Movie movie)
-//    {
-//        InitializeComponent();
-//        _movie = movie;
-//    }
-//    public Movie Movie()
-//    {
-//        return _movie;
-//    }
-//}
+        //show all the movie details in the moviePage.xaml
+        //1: create UI: image, label(s), and a button 
+        //2: populate the UI with the received object
+
+        MovieImage.Source = _movies.ImageUrl;
+        MovieName.Text = _movies.Name;
+        MovieDuration.Text = "Duration: " + _movies.Duration.ToString() + " mins";
+        MovieGenre.Text = "Genre: "+_movies.Genre;
+        MovieMainActor.Text = "Actor: "+_movies.MainActor;
+        MovieLanguage.Text = "Language: "+_movies.Language;
+        
+    }
+
+    void BuyTickets_Clicked(object sender, EventArgs e)
+    {
+       
+      //  Navigation.PushAsync(new BookTicket());
+    }
+}
