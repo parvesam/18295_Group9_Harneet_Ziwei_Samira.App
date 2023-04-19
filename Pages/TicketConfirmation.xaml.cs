baeef2e8ee774afea58c7eb1105db742c2bf00c8
ziwei zhao <zhaoziwei0@gmail.com>
2023-04-18 23:34:13 -0400

    Update TicketConfirmation.xaml.cs

--- Pages/TicketConfirmation.xaml.cs
+++ Pages/TicketConfirmation.xaml.cs
@@ -1,9 +1,15 @@
+using _18295_Group9_Harneet_Ziwei_Samira.Models;
 namespace _18295_Group9_Harneet_Ziwei_Samira.Models;
 
 public partial class TicketConfirmation : ContentPage
 {
-	public TicketConfirmation()
+	public TicketConfirmation(Ticket ticket, Hall hall)
 	{
 		InitializeComponent();
+		 MovieLabel.Text = ticket.MovieName;
+        	CinemaLebal.Text = ticket.Location;
+        	HallNameLabel.Text = hall.HallName;
+        	// ScheduleLabel.Text= schedule.DateTime.ToString();
+        	// SeatsLabel.Text = ;
 	}
 }
