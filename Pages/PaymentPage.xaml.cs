67eaf9f91427af9b8cb0f45cd502acc9811ec6ac
ziwei zhao <zhaoziwei0@gmail.com>
2023-04-20 23:12:35 -0400

    Update PaymentPage.xaml.cs

--- Pages/PaymentPage.xaml.cs
+++ Pages/PaymentPage.xaml.cs
@@ -1,17 +1,49 @@
 namespace _18295_Group9_Harneet_Ziwei_Samira.Pages;
 using _18295_Group9_Harneet_Ziwei_Samira.Models;
+using System;
 
 public partial class PaymentPage : ContentPage
 {
-	public PaymentPage()
+    private bool emailOptionSelected;
+
+    public PaymentPage()
 	{
 		InitializeComponent();
-        //LocationListView.ItemsSource = ScheduleRepository.SearchByName(movie.Name);
     }
 
-    void Confirmed_Clicked(object sender, EventArgs e)
+    void OnConfirmClicked(object sender, EventArgs e)
     {
-        //var selectedticket = LocationListView.SelectedItem as ticket;
-        //Navigation.PushAsync(new TicketConfirmation());
+        if (CardNumberEntry.Text.Length != 10)
+        {
+            DisplayAlert("Error", "Incorrect card number. Please enter a 10-digit card number.", "OK");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(NameEntry.Text))
+        {
+            DisplayAlert("Error", "Please enter your name.", "OK");
+            return;
+        }
+
+        if (!string.IsNullOrWhiteSpace(EmailEntry.Text) && !IsValidEmail(EmailEntry.Text))
+        {
+            DisplayAlert("Error", "Please enter a valid email address.", "OK");
+            return;
+        }
+
+         Navigation.PushAsync(new TicketConfirmation(emailOptionSelected));
+    }
+
+    bool IsValidEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
